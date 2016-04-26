86f55f75fb80010fab2259875ffa11dec1f112f3
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2016-04-26 22:21:45 +0600

    Front-end DONE

---
+++ assets/js/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('todoApp', ['ngRoute', 'ui.bootstrap'])
+
+  .config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider.when('/', {
+        templateUrl: '/templates/todo.html',
+        controller: 'TodoCtrl'
+      }).otherwise({
+        redirectTo: '/',
+        caseInsensitiveMatch: true
+      });
+
+    }])
+
+  .controller('TodoCtrl', ['$scope', '$rootScope', 'TodoService', function ($scope, $rootScope, TodoService) {
+    $scope.formData = {};
+    $scope.todos = [];
+
+    TodoService.getTodos().then(function (response) {
+      $scope.todos = response;
+    });
+
+    $scope.addTodo = function () {
+      TodoService.addTodo($scope.formData).then(function (response) {
+        $scope.todos.push($scope.formData);
+        $scope.formData = {};
+      });
+    };
+
+    $scope.removeTodo = function (todo) {
+      TodoService.removeTodo(todo).then(function (response) {
+        $scope.todos.splice($scope.todos.indexOf(todo), 1)
+      });
+    };
+  }]);
