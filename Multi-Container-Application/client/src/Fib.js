285dc22fc77f4a5a0d4889e8497662ee9d483329
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-10-15 15:35:35 +0600

    CI Workflow for Multiple Images

--- Multi-Container-Application/client/src/Fib.js
+++ Multi-Container-Application/client/src/Fib.js
@@ -20,9 +20,7 @@ class Fib extends Component {
 
     async fetchIndexes() {
         const seenIndexes = await axios.get('/api/values/all');
-        this.setState({
-            seenIndexes: seenIndexes.data
-        });
+        this.setState({ seenIndexes: seenIndexes.data });
     }
 
     handleSubmit = async event => {
